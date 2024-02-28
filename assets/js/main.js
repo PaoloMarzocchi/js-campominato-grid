004c1701e7d508c24d4cfc8a425f30547fdfa8b8
PaoloMarzocchi <marzocchi.3p@gmail.com>
2024-02-28 16:57:18 +0100

    add color change and print number cell by clicking on the cells

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 //- creo dinamicamente la griglia di gioco al click sul bottone
+//- inserisco in ogni cella un numero progressivo da 1 a 100
 
 const containerEl = document.querySelector('.container');
 let numbCells = 100;
@@ -9,16 +10,32 @@ function gridGenerator() {
         let markup = `<div class="cell">${i}</div>`;
         containerEl.insertAdjacentHTML('beforeend', markup);
     }
-    return containerEl.querySelectorAll('div.cell')
 }
 //console.log(gridGenerator());
 
-btnGeneratorEl.addEventListener('click', function(){
+btnGeneratorEl.addEventListener('click', function (e) {
+
     gridGenerator();
     //console.log(gridGenerator());
-})
-let cellEl = containerEl.querySelectorAll('div.cell');
-//- inserisco in ogni cella un numero progressivo da 1 a 100
-//- creo l'evento al click sulle celle 
+
+
+
+    //- creo l'evento al click sulle celle 
     //- stampo in console il suo numero
     //- aggiungo dinamicamente una classe alla cella cliccata per cambiare il suo colore
+    
+    for (let i = 1; i <= numbCells; i++) {
+        let cellEl = document.querySelectorAll('div.cell');
+        //console.log(cellEl);
+        cellEl[i - 1].addEventListener('click', function () {
+            cellEl[i - 1].classList.toggle('red');
+            console.log(i);
+        })
+    }
+
+})
+
+
+
+
+
