5f6fe4605fd25021830d007c6dc2581fa1859e90
PaoloMarzocchi <marzocchi.3p@gmail.com>
2024-02-29 18:44:11 +0100

    add refactoring

--- campo-minato_updated/assets/js/main.js
+++ campo-minato_updated/assets/js/main.js
@@ -1,76 +1,44 @@
-// - inserisco i vari numeri appena creati casualmente nelle celle
-// - se una di queste celle viene cliccata la coloro di rosso, se viene cliccata una cella senza il numero all'interno la coloro di azzurro
-// - quando la cella cliccata ha un numero dell'array dei funghi blocco la partita
-// - stampo l'esito della partita ed il punteggio(contando le volte che ha cliccato su celle vuote) 
 const containerEl = document.querySelector('.container');
 let numbCells;
 const generatorEl = document.querySelector('form');
-// - Creo un array per i 16 numeri dove saranno posizionati i funghi
-
-//console.log(ninjasArray);
-
-
-/* containerEl.innerHTML = "";
-    for (let i = 1; i <= 100; i++) {
-        let markup = `<div class="cell grid_100">${i}</div>`;
-        containerEl.insertAdjacentHTML('beforeend', markup);
-    }
-    let cellEl = document.querySelectorAll('div.cell');
-    //console.log(cellEl); */
-
-
-/* function randomNinjasCell() {
-    
-} */
-/*  const ninjasArray = ninjaArrayNumbers();
- //console.log(randomNumbVar);
- for (let i = 0; i < cellEl.length; i++) {
-     let cell = cellEl[i];
-     let cellNumb = Number(cell.innerHTML)
-     
-     if (ninjasArray.includes(cellNumb)) {
-         cell.classList.toggle('red');
-     } else{
-         cell.classList.toggle('blue');
-     }
- }   
-*/
-
-/* if(!cellEl.innerHTML.includes(ninjaCell)) {
-    //console.log(cellEl.innerHTML);
-    cellEl.innerHTML = ninjaCell;
-} else{
-    element.innerHTML = "";
-} */
-
-//}
-
-//console.log(ninjasArray);
 
 
 //- creo dinamicamente la griglia di gioco al click sul bottone
 generatorEl.addEventListener('submit', function (e) {
     e.preventDefault();
 
+    let endGameMessage = document.querySelector('h2');
+    endGameMessage.innerHTML = ``;
+    endGameMessage.style.backgroundColor = 'transparent';
+
     const levelValue = document.getElementById('level').value
     const gridCells = level(levelValue);
     gridGenerator(gridCells);
-    const ninjasArray = ninjaArrayNumbers();
+
+    // - Creo un array per i 16 numeri dove saranno posizionati i funghi
+    const ninjasArray = ninjaArrayNumbers(gridCells);
     flag = true;
     counter = 0;
+
     //- creo l'evento al click sulle celle 
+
     for (let i = 0; i < gridCells; i++) {
         let cellEl = document.querySelectorAll('div.cell');
         cellEl[i].addEventListener('click', function () {
+            let endGameMessage = document.querySelector('h2');
             if (flag) {
-                //console.log(flag);
+
                 this.innerHTML = i + 1;
                 let cellNumb = Number(this.innerHTML)
-                console.log(ninjasArray);
+
+                // - confronto i numeri dell'array casuale con quelli della griglia
+                // - se una di queste celle viene cliccata la coloro di rosso, se viene cliccata una cella senza il numero all'interno la coloro di azzurro
                 if (ninjasArray.includes(cellNumb)) {
                     this.classList.add('red');
                     this.innerHTML = '🐱‍👤';
+                    // - quando la cella cliccata ha un numero dell'array dei funghi blocco la partita
                     flag = false;
+
                 } else {
                     this.classList.add('blue');
                     counter++;
@@ -78,14 +46,20 @@ generatorEl.addEventListener('submit', function (e) {
                     console.log(ninjasArray.length);
 
                 }
-            }  else {
-                document.querySelector('.container').innerHTML = `<h1>HAI PERSO! N° MOSSE: ${counter}</h1>`
+            } else {
+                // - stampo l'esito della partita ed il punteggio(contando le volte che ha cliccato su celle vuote) 
+
+                endGameMessage.innerHTML = `<h1>HAI PERSO! N° MOSSE: ${counter}</h1>`;
+                endGameMessage.style.backgroundColor = 'darkred';
                 console.log(counter);
 
-            } 
+            }
+
             const winCells = cellEl.length - ninjasArray.length;
+
             if (counter == winCells) {
-                document.querySelector('.container').innerHTML = `<h1>HAI VINTO!</h1>`
+                endGameMessage.innerHTML = `<h1>HAI VINTO!`;
+                endGameMessage.style.backgroundColor = 'chartreuse';
                 console.log(counter);
                 //- stampo in console il suo numero
                 //console.log(i + 1);
@@ -94,15 +68,10 @@ generatorEl.addEventListener('submit', function (e) {
     }
 })
 
-/* else if (counter === (cellEl.length - ninjasArray.length)) {
-    document.querySelector('.container').innerHTML = `<h1>HAI VINTO</h1>`
-    console.log(counter);
-    //- stampo in console il suo numero
-    //console.log(i + 1);
-} */
+
 
 /**
- * 
+ * Genera la griglia di gioco
  * @param {number} numMaxCells 
  */
 function gridGenerator(numMaxCells) {
@@ -113,13 +82,13 @@ function gridGenerator(numMaxCells) {
     }
 }
 /**
- * 
+ * imposta il livello selezionato dall'utente
  * @param {string} lvl 
  * @returns {number}
  */
 function level(lvl) {
     switch (lvl) {
-        
+
         case 'easy':
             numbCells = 100;
             break;
@@ -133,24 +102,24 @@ function level(lvl) {
     return numbCells
 }
 /**
- * 
+ * Genera un numero casuale 
  * @param {*} min 
  * @param {*} max 
- * @returns 
+ * @returns {number}
  */
 function randomNumb(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 /**
+ * Crea un array di 16 numeri casuali
  * @param {number} maxCells
- * @returns {Array}
+ * @returns {Array} 
  */
 function ninjaArrayNumbers(maxCells) {
 
     const ninjasArray = [];
-    const ninjaMaxNum = 100;
     while (ninjasArray.length < 16) {
-        const randomNumbVar = randomNumb(1, ninjaMaxNum);
+        const randomNumbVar = randomNumb(1, maxCells);
         // - Controllo che nell'array i numeri creati non siano ripetuti
         if (!ninjasArray.includes(randomNumbVar)) {
             ninjasArray.push(randomNumbVar);
